50e0144f81167c93f6bd1be6050af963748fe487
Alexey Berezhnoy <bobrokot1488@gmail.com>
2016-02-27 10:01:19 +0300

    добавлены валидаторы

--- assessment/models.py
+++ assessment/models.py
@@ -1,10 +1,16 @@
 from django.db import models
+from django.conf import settings
+from assessment.validator import *
+
+CATEGORIES = ['A', 'A1', 'B', 'B1', 'BE', 'C', 'C1', 'CE', 'C1E', 'D', 'D1', 'DE', 'D1E', 'M', 'Tm', 'Tb']
 
 
 class Person(models.Model):
-    last_name = models.CharField(max_length=30)
-    first_name = models.CharField(max_length=30)
-    patronymic = models.CharField(max_length=30)
+
+    last_name = models.CharField(max_length=30, validators=[validate_name])
+    first_name = models.CharField(max_length=30, validators=[validate_name])
+    patronymic = models.CharField(max_length=30, validators=[validate_name])
+    user = models.OneToOneField(settings.AUTH_USER_MODEL)
 
     def __str__(self):
         return "%s %s.%s." % (self.last_name, self.first_name[0], self.patronymic[0])
@@ -14,29 +20,35 @@ class Person(models.Model):
 
 
 class Expert(Person):
-    CATEGORIES = ['A', 'A1', 'B', 'B1', 'BE', 'C', 'C1', 'CE', 'C1E', 'D', 'D1', 'DE', 'D1E', 'M', 'Tm', 'Tb']
 
-    profession = models.CharField(max_length=30)
-    position = models.CharField(max_length=30)
-    professional_experience = models.IntegerField()
+    profession = models.CharField(max_length=30, validators=[validate_profession])
+    position = models.CharField(max_length=30, validators=[validate_profession])
+    professional_experience = models.PositiveSmallIntegerField(validators=[validate_experience])
     driver_license = models.CharField(max_length=30,
                                       blank=True,
                                       choices=((cat, cat) for cat in CATEGORIES))
-    driving_experience = models.IntegerField(blank=True)
+    driving_experience = models.IntegerField(blank=True, validators=[validate_experience])
+
+    def save(self, *args, **kwargs):
+        if not self.driver_license:
+            self.driving_experience = False
+        super(Expert, self).save(*args, **kwargs)
 
 
 class Quality(models.Model):
+
     quality = models.CharField(max_length=60)
     description = models.TextField()
-
-    def get_assessment(self):
-        pass
 
     def __str__(self):
         return self.quality
 
 
 class Assessment(models.Model):
+
     quality = models.ForeignKey('Quality')
     expert = models.ForeignKey('Expert')
-    point = models.IntegerField()
+    point = models.PositiveSmallIntegerField(validators=[validate_point])
+
+    class Meta:
+        unique_together = ("quality", "expert")
