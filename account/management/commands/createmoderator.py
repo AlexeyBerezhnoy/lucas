aef2c97c1a5e5403c45ece3b4dc3908cd16a0663
Alexey Berezhnoy <bobrokot1488@gmail.com>
2016-07-15 06:40:40 +0300

    реализована команда добавления модератора

--- account/management/commands/createmoderator.py
+++ account/management/commands/createmoderator.py
@@ -1,8 +1,67 @@
+from django.core.exceptions import ValidationError
 from django.core.management.base import BaseCommand
-from account.models import MyUser
+from django.template.loader import render_to_string
+from account.models import Moderator
+from account.validator import validate_name
+from django.core.validators import validate_email
+import getpass
+from account.views import get_new_password
+from account.views import send_message
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        self.stdout.write('hii')
+        while True:
+            self.stdout.write('Email:', ending=" ")
+            email = input()
+            try:
+                validate_email(email)
+            except ValidationError:
+                self.stderr.write("email невалиден")
+            else:
+                break
 
+        while True:
+            self.stdout.write('Фамилия:', ending=" ")
+            last_name = input()
+            try:
+                validate_name(last_name)
+            except ValidationError:
+                self.stderr.write("Данные невалидны")
+            else:
+                break
+
+        while True:
+            self.stdout.write('Имя:', ending=" ")
+            first_name = input()
+            try:
+                validate_name(first_name)
+            except ValidationError:
+                self.stderr.write("Данные невалидны")
+            else:
+                break
+
+        while True:
+            self.stdout.write('Отчество:', ending=" ")
+            middle_name = input()
+            try:
+                validate_name(middle_name)
+            except ValidationError:
+                self.stderr.write("Данные невалидны")
+            else:
+                break
+
+        Moderator.objects.create_moderator(email=email,
+                                           first_name=first_name,
+                                           middle_name=middle_name,
+                                           last_name=last_name)
+
+        moderator = Moderator.objects.get(email=email)
+        password = get_new_password()
+        moderator.set_password(password)
+        moderator.save()
+
+        subject = "Добро пожаловать"
+        ctx = {'user': moderator, 'password': password}
+        message = render_to_string('account/email/invite_moderator.html', ctx)
+        send_message(moderator.email, subject, message)
