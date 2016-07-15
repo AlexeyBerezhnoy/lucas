0450b3b3807837088bd358539a2c54d5917817f4
Alexey Berezhnoy <bobrokot1488@gmail.com>
2016-07-15 03:57:58 +0300

    Удалено приложение админки

--- lucas/settings.py
+++ lucas/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = ['lucaspistoletov.pythonanywhere.com']
 # Application definition
 
 INSTALLED_APPS = (
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
